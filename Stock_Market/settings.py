5a82334d9eca4f9e62a302247e0c4086ddc76cee
Farheen <farheenriyazi14@gmail.com>
2021-08-27 17:12:38 +0530

    static file added its 132 files automatic generated

--- Stock_Market/settings.py
+++ Stock_Market/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # adding media for saving the videos
 MEDIA_URL = '/media/'
